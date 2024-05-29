2628bd45f66f95304b013e1751642cc6dcec09a6
oyatillo0509 <tilloxon0509@gmail.com>
2024-05-29 12:59:06 +0500

    finished

---
+++ js/index.js
@@ -0,0 +1,166 @@
+// 1.1-masala
+// let n = +prompt("soni kiriting");
+// for (let i = 1; i <= n; i++) {
+//   console.log(i);
+// }
+
+// 1.2-masala
+// let n = +prompt("soni kiriting");
+// for (let i = n; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// 1.3-masala
+// let n = +prompt("soni kiriting");
+// let y = 0;
+// for (let i = 1; i <= n; i++) {
+//   y += i;
+// }
+// console.log(y);
+
+// 1.4-masala
+// let n = +prompt("soni kiriting");
+// let y = 0;
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2 === 0) {
+//     y += i;
+//     console.log(i);
+//   }
+// }
+// console.log(y);
+
+// 1.5-masala
+// let n = +prompt("soni kiriting");
+// let y = 0;
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2 === 1) {
+//     y += i;
+//     console.log(i);
+//   }
+// }
+// console.log(y);
+
+// 1.6-masala
+// let n = +prompt("n soniga qiymat kiriting");
+// for (let i = 1; i <= n; i++) {
+//   if (n % i === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 1.7-masala
+// let n = +prompt("soni kiriting");
+// let a = 0;
+// let y = 0;
+// for (let i = 1; i <= n; i++) {
+//   if (n % i === 0) {
+//     console.log(i);
+//     a ++;
+//     y += i;
+//   }
+// }
+// console.log(a);
+// console.log(y);
+
+// 1.8-masala
+// let n = +prompt("soni kiriting");
+// let a = 0;
+// for (let i = 1; i <= n; i++) {
+//   if (n % i === 0) {
+//     a++;
+//   }
+// }
+// if (a === 2) {
+//   console.log("tub son");
+// } else {
+//   console.log("tub son emas");
+// }
+
+// 2.1-masala
+// chunmadim
+
+// 2.2-masala
+// let a = +prompt("a soni kiriting");
+// let b = +prompt("b soni kiriting");
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+// }
+
+// 2.3-masala
+// let a = +prompt("a soni kiriting");
+// let b = +prompt("b soni kiriting");
+// for (let i = b; i >= a; i--) {
+//   console.log(i);
+// }
+
+// 2.4-masala
+// let narx = 75000;
+// for (let i = 1; i <= 10; i++) {
+//   let y_narx = i * narx;
+//   console.log(y_narx);
+// }
+
+// 2.5-masala
+// let narx = 75000;
+// for (let i = 0.1; i <= 1; i += 0.1) {
+//   console.log(narx * i);
+// }
+
+// 2.6-masala
+// let price = 50000;
+// for (let i = 1.2; i <= 2; i += 0.2) {
+//   console.log(price * i);
+// }
+
+// 2.7-masala
+// let a = +prompt("a soni kiriting");
+// let b = +prompt("b soni kiriting");
+// let y = 0;
+// for (let i = a; i <= b; i++) {
+//   y += i;
+// }
+// console.log(y);
+
+// 2.8-masala
+// let a = +prompt("a soni kiriting");
+// let b = +prompt("b soni kiriting");
+
+// let y = 1;
+// for (let i = a; i <= b; i++) {
+//   y *= i;
+// }
+// console.log(y);
+
+// 2.9-masala
+// let a = +prompt("a soni kiriting");
+// let b = +prompt("b soni kiriting");
+
+// let y = 1;
+// for (let i = a; i <= b; i++) {
+//   y *= i ** 2;
+// }
+// console.log(y);
+
+// 2.10-masala
+// let n = +prompt("soni kiriting");
+// let y = 0;
+// for (let i = 1; i <= n; i++) {
+//   y += 1 / i;
+// }
+// console.log(y);
+
+// 2.11-masala
+// let n = +prompt("soni kiriting");
+// let y = 1;
+// for (let i = 1; i <= n; i++) {
+//   y += (n + i) ** 2;
+// }
+// console.log(y);
+
+// 2.12-masala
+// let n = +prompt("soni kiriting");
+// let y= 1;
+// for (let i = 1.1; i <= n; i += 0.1) {
+//   y *= i;
+// }
+// console.log(y);
